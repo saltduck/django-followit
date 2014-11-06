e2d4fa2e326e5272ebd50428f3d668de5eb4d233
Hong Shuning <hongshuning@gmail.com>
2014-11-06 16:55:27 +0800

    Update runtests.py

--- followit/tests/runtests.py
+++ followit/tests/runtests.py
@@ -1,5 +1,6 @@
 import os, sys
-os.environ['DJANGO_SETTINGS_MODULE'] = '.followit.tests.settings'
+sys.path.append('.')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'followit.tests.settings'
 from django.test.runner import DiscoverRunner
 failures = DiscoverRunner().run_tests(['tests.FollowerTests',], verbosity = 1)
 if failures:
